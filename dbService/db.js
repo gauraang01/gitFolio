90359c3985b6539f2739e54b1927ef4c8d0deb37
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2023-04-15 18:46:20 +0530

    Changes in db

--- dbService/db.js
+++ dbService/db.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB database
-const username = process.env.MONGO_INITDB_ROOT_USERNAME;
-const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const endpoint = 'mongodb:27017'; // change this to the actual endpoint of your MongoDB container
 
-const databaseName = 'myDatabase';
-const uri = `mongodb://${username}:${password}@${endpoint}/${databaseName}`;
+// const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+// const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+// const endpoint = 'mongodb:27017'; // change this to the actual endpoint of your MongoDB container
+
+// const databaseName = 'myDatabase';
+// const uri = `mongodb://${username}:${password}@${endpoint}/${databaseName}`;
 
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('mongodb://localhost:27017/resumeDB');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
